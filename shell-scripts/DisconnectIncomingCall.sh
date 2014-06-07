9341feb6d16b4c4131155b2a60aa6c47d5521b5a
Srayan Guhathakurta <srayanguhathakurta@yahoo.com>
2014-06-07 18:38:42 -0500

    Create DisconnectIncomingCall.sh

---
+++ shell-scripts/DisconnectIncomingCall.sh
@@ -0,0 +1,11 @@
+# Swiping the screen to reject the incoming call
+adb shell input swipe 398 969 105 986
+
+# To go back to home
+adb shell input keyevent 4
+adb shell input keyevent 4
+
+# For locking the phone screen
+adb shell input keyevent 26
+
+
